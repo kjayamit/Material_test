3600ed033666764f8b7b55f00d6bedd8b4ec37f4
Jaya Chakravarthi Kuppu <jayachk@thoughtworks.com>
2016-11-24 13:38:39 +0530

    Added custom toolbar and menu

--- app/src/main/java/com/example/jaya/materialtest/SubActivity.java
+++ app/src/main/java/com/example/jaya/materialtest/SubActivity.java
@@ -1,6 +1,7 @@
 package com.example.jaya.materialtest;
 
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,24 +10,27 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class SubActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub);
 
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
+        inflater.inflate(R.menu.menu_sub, menu);
         return true;
     }
 
@@ -36,13 +40,12 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if(id == R.id.action_settings){
-            Toast.makeText(this, "Hey you hit "+ item.getTitle(),Toast.LENGTH_SHORT).show();
             return true;
         }
 
-        if(id == R.id.navigate){
-           startActivity(new Intent(this, SubActivity.class));
+        if(id == android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
         }
-        return super.onOptionsItemSelected(item);
+        return super. onOptionsItemSelected(item);
     }
 }
