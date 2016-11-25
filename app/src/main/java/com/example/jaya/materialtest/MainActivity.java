5ffbca2fa2c3c4f28aac5caa6a78807f5a7b7d3d
Jaya Chakravarthi Kuppu <jayachk@thoughtworks.com>
2016-11-25 22:20:48 +0530

    Navigation part 4

--- app/src/main/java/com/example/jaya/materialtest/MainActivity.java
+++ app/src/main/java/com/example/jaya/materialtest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.jaya.materialtest;
 
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,6 +14,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private  NavigationDrawerFragment drawerFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+       drawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+
+        drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout) findViewById(R.id.drawer_layout),toolbar);
     }
 
     @Override
@@ -42,6 +52,12 @@ public class MainActivity extends AppCompatActivity {
 
         if(id == R.id.navigate){
            startActivity(new Intent(this, SubActivity.class));
+        }
+
+
+        if(id == android.R.id.home){
+//            drawerFragment.getActivity().;
+            Toast.makeText(this, "how to ??? "+ item.getTitle(),Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
     }
