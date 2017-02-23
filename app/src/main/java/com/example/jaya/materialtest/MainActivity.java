06c9389f2f445a454b6be2b160bdca32fd2f2b20
Jaya Chakravarthi Kuppu <jayachk@thoughtworks.com>
2017-02-24 00:07:51 +0530

    Jaya: 223 added image and status bar customization part 1

--- app/src/main/java/com/example/jaya/materialtest/MainActivity.java
+++ app/src/main/java/com/example/jaya/materialtest/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_appbar);
+        setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
