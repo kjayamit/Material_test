e71640bf631abde8e52242ec5e3c1d6036fcc62f
Jaya Chakravarthi Kuppu <jayachk@thoughtworks.com>
2017-03-02 23:02:09 +0530

    Jaya: 228 recycler view for navigation drawer

---
+++ app/src/main/java/com/example/jaya/materialtest/Information.java
@@ -0,0 +1,11 @@
+package com.example.jaya.materialtest;
+
+/**
+ * Created by jaya on 28/02/17.
+ */
+
+public class Information {
+
+    int iconID;
+    String title;
+}
--- app/src/main/java/com/example/jaya/materialtest/NavigationDrawerFragment.java
+++ app/src/main/java/com/example/jaya/materialtest/NavigationDrawerFragment.java
@@ -7,10 +7,15 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -28,6 +33,10 @@ public class NavigationDrawerFragment extends Fragment {
     private boolean mFromSavedInstanceState;
 
     private View containerView;
+
+    private RecyclerView recyclerView;
+
+    private VizAdapter adapter;
 
     public NavigationDrawerFragment() {
         // Required empty public constructor
@@ -49,7 +58,27 @@ public class NavigationDrawerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        View layout = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+
+        recyclerView = (RecyclerView) layout.findViewById(R.id.drawerList);
+
+        adapter = new VizAdapter(getActivity(),getData());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return layout;
+    }
+
+    public static List<Information> getData(){
+        List<Information> data = new ArrayList<>();
+        int[] icons={ R.drawable.checkbox24, R.drawable.mancycling,R.drawable.filterresults};
+        String[] title = {"one","two","three"};
+        for(int i=0;i<title.length;i++){
+            Information current = new Information();
+            current.iconID = icons[i];
+            current.title = title[i];
+            data.add(current);
+        }
+        return data;
     }
 
     public void setUp(int fragmentID, DrawerLayout drawerLayout, final Toolbar toolbar) {
